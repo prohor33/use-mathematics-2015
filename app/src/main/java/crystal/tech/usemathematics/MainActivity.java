741d03266a1861320d61fbfddc71ab196a7643b5
Gladkikh Prohor <prohor33@gmail.com>
2015-03-19 01:04:42 +0300

    start to implement main activity, it's **cking horrible

--- app/src/main/java/crystal/tech/usemathematics/MainActivity.java
+++ app/src/main/java/crystal/tech/usemathematics/MainActivity.java
@@ -1,9 +1,15 @@
 package crystal.tech.usemathematics;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +18,23 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        WebView webView = (WebView) findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setPluginState(WebSettings.PluginState.ON);
+        String videoID = "DGKwdHMiqCg";
+        webView.loadUrl("http://www.youtube.com/embed/" + videoID + "?autoplay=1&vq=small");
+        webView.setWebChromeClient(new WebChromeClient());
+
+        Button buttonGo = (Button) findViewById(R.id.buttonGo);
+        buttonGo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MainTopicListActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 

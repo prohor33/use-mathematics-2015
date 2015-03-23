be030f06c803e4d61fa86407a5d582ef1d1d0cd8
Gladkikh Prohor <prohor33@gmail.com>
2015-03-24 00:46:01 +0300

    start to implement theme.html

--- www/js/exercise.js
+++ www/js/exercise.js
@@ -45,9 +45,5 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
-    },
-
-    goStartPage: function() {
-        window.location = "index.html";
     }
 };

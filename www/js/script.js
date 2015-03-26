031937dc9655da7ee597fa2810744e242481a1b0
Gladkikh Prohor <prohor33@gmail.com>
2015-03-26 22:27:11 +0300

    change back button function

--- www/js/script.js
+++ www/js/script.js
@@ -1,10 +1,6 @@
 var toolbar = {
 	parent_page: "",
-	setParent: function(parent_page) {
-		this.parent_page = parent_page;
-	},
 	onBack: function() {
-		window.location = this.parent_page;
-		console.log("onBack() - " + this.parent_page);
+  	navigator.app.backHistory();
 	}
 };

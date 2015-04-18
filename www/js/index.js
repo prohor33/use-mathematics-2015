b2178f20f50808965a3a36dfb01873be54a77215
Gladkikh Prohor <prohor33@gmail.com>
2015-04-19 01:13:39 +0300

    fix #7 image instead of the video on index page if no internet

--- www/js/index.js
+++ www/js/index.js
@@ -2,14 +2,28 @@
  * Copyright (c) 2014-2015 Crystal Tech. All rights reserved.
  */
 
-var connection = true
+var connection = false;
+
+document.addEventListener("deviceready", onDeviceReady, false);
+function onDeviceReady() {
+    // Now safe to use device APIs
+    // alert("devide is ready");
+    checkConnection();
+
+    setTimeout(function() {
+        if (connection) {
+            show_video();
+        }
+    }, 550);
+}
 
 function checkConnection() {
+    // alert("0");
     try {
         var networkState = navigator.connection && navigator.connection.type;
 
         setTimeout(function(){
-            // networkState = navigator.connection && navigator.connection.type;
+            networkState = navigator.connection && navigator.connection.type;
 
             // var states = {};
             // states[Connection.UNKNOWN]  = 'Unknown connection';
@@ -19,8 +33,8 @@ function checkConnection() {
             // states[Connection.CELL_3G]  = 'Cell 3G connection';
             // states[Connection.CELL_4G]  = 'Cell 4G connection';
             // states[Connection.NONE]     = 'No network connection';
-
             // alert('Connection type: ' + states[networkState]);
+
             connection = !(networkState == Connection.NONE || networkState == Connection.UNKNOWN);
         }, 500);
     } catch(e) {
@@ -38,41 +52,45 @@ function img_on_click() {
     var video = document.getElementById("video");
 
     // doesn't work??
-    alert("1");
+    // alert("1");
     // set to either landscape
-    alert('Orientation is ' + screen.orientation);
-    alert(screen.width);
-    screen.lockOrientation('landscape');
+    // alert('Orientation is ' + screen.orientation);
+    // alert(screen.width);
+    // screen.lockOrientation('landscape');
     // var so = cordova.plugins.screenorientation;
-    alert("2");
-    so.setOrientation(so.Orientation.LANDSCAPE);
-    alert("set orination to landscape");
+    // alert("2");
+    // so.setOrientation(so.Orientation.LANDSCAPE);
+    // alert("set orination to landscape");
 
-    video.addEventListener("playing", function() {
-        alert("event");
-        console.log("event");
-        var so = cordova.plugins.screenorientation;
-        so.setOrientation(so.Orientation.LANDSCAPE);
-    }, false);
+    // video.addEventListener("playing", function() {
+    //     alert("event");
+    //     console.log("event");
+    //     var so = cordova.plugins.screenorientation;
+    //     so.setOrientation(so.Orientation.LANDSCAPE);
+    // }, false);
     
 
-    checkConnection();
+    // checkConnection();
 
-    setTimeout(function() {
-        if (!connection) {
-            alert("Есть проблемы с интернет соединением, пожалуйста, проверьте настройки подключения");
-            return;
-        }
-        show_video();
-    }, 550);
+    // setTimeout(function() {
+    //     if (!connection) {
+    //         alert("Есть проблемы с интернет соединением, пожалуйста, проверьте настройки подключения");
+    //         return;
+    //     }
+    //     show_video();
+    // }, 550);
+
+    if (!connection) {
+        alert("Есть проблемы с интернет соединением, пожалуйста, проверьте настройки подключения");
+        return;
+    }
+
+    show_video();
 }
 
 function show_video() {
     var youtube_player = document.getElementById('youtube_player');
     var video_image = document.getElementById('video_image');
-    // TODO: make it autoplay
-    youtube_player.innerHTML = "<iframe id=\"video\" class=\"video\" frameborder=\"0\"\
-    src=\"http://www.youtube.com/embed/MMC0iaz6bac?autoplay=1&hd=1&rel=0&autohide=1&showinfo=0\"></iframe>";
     youtube_player.style.display = 'initial';
     video_image.style.display = 'none';
 }
@@ -92,6 +110,9 @@ function switch_pages(to_index) {
     document.body.style.backgroundImage = to_index ? 'url(img/back_squared.png)' : 'none';
     // alert("0");
     document.body.style.overflow = to_index ? "hidden" : "inherit";
+    var htmlElement = document.getElementsByTagName("html")[0];
+    // doesn't work???
+    htmlElement.style.overflow = to_index ? "hidden" : "inherit";
 
     var back = document.getElementsByClassName('back')[0];
     back.style.display = !to_index ? 'inherit' : 'none';
--- www/js/index_start.js
+++ www/js/index_start.js
@@ -8,12 +8,14 @@ function on_load() {
     var load_index = true;
 
     var result = parse("theme_list_page");
+    // uncomment
     if (result != "Not found")
         load_index = false;
 
     // alert("0");
     if (load_index) {
-        document.write('<style>.theme_list_page { display: none; }\nbody { background-image: url("img/back_squared.png");\noverflow: hidden; }</style>');
+        document.write('<style>.theme_list_page { display: none; }\nbody { background-image: url("img/back_squared.png");\noverflow: hidden; }\n\
+            html { overflow: hidden; }</style>');
     } else {
         document.write('<style>.index_page { display: none; }</style>');
     }

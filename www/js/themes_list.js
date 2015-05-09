db5b4fa00bd984109aefae441497ecebb59d7dee
Gladkikh Prohor <prohor33@gmail.com>
2015-05-09 15:12:33 +0300

    pause youtube video when app in background because that was violation of YouTube policy

--- www/js/themes_list.js
+++ www/js/themes_list.js
@@ -2,6 +2,26 @@
     Copyright (c) 2014-2015 Crystal Tech. All rights reserved.
  */
 
+// alert("1");
+inappbilling.init(successInit,errorCallback, {showLog:true})
+// alert("2");
+
+function successInit(result) {
+    // display the extracted text   
+    alert(result); 
+    // make the purchase
+    inappbilling.buy(successPurchase, errorCallback, "full_content_access");
+
+}    
+function errorCallback(error) {
+   alert(error); 
+} 
+
+function successPurchase(productId) {
+   alert("Your item has been purchased!");
+}
+
 function locked() {
     alert("Медиа контент для данной задачи находится в стадии разработки");
+
 }
--- www/js/video.js
+++ www/js/video.js
@@ -105,3 +105,18 @@ function show_video() {
     video_image.style.display = 'none';
 }
 
+function toggleVideo(state) {
+    // if state == 'hide', hide. Else: show video
+    var div = document.getElementById("youtube_player");
+    var iframe = div.getElementsByTagName("iframe")[0].contentWindow;
+    func = state == 'hide' ? 'pauseVideo' : 'playVideo';
+    iframe.postMessage('{"event":"command","func":"' + func + '","args":""}','*');
+}
+
+document.addEventListener("pause", onPause, false);
+
+function onPause() {
+    // Handle the pause event
+    toggleVideo('hide');
+}
+
